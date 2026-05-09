de686cc6208b70ba9c317c4f3d457416c3e53179
Yesebu <yesebumylapalli08@gmail.com>
2026-05-09 07:18:48 +0530

    configure Maven project with executable JAR and backend structure

---
+++ src/main/java/com/codeverdict/Main.java
@@ -0,0 +1,13 @@
+package com.codeverdict;
+
+/**
+ * Application entry point for CodeVerdict.
+ * Bootstraps the HTTP server and registers all routes.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("CodeVerdict starting...");
+        // TODO: initialise DB connection, register routes, start server
+    }
+}
